a85b1d8a74788a782f8a360b0b443ca258439ed7
rflorian2017 <roby_florian@yahoo.com>
2019-04-13 12:13:07 +0300

    Second commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public MenuItem mnuItmChangeCategories;
+    public TextField txtFieldUserName;
+    public PasswordField passwordFieldPwd;
+    public Button btnLogin;
+
+    public void loginAction(ActionEvent event) {
+        if(btnLogin.getText().equals("Login")) {
+            btnLogin.setText("Logout");
+        }
+
+        else {
+            btnLogin.setText("Login");
+        }
+    }
 }
