b8b0ef1980e3c6bb70f486148bcf082ad678248e
rflorian2017 <roby_florian@yahoo.com>
2019-04-13 12:36:04 +0300

    3rd commit

---
+++ src/constants/ApplicationConstants.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class ApplicationConstants {
+    public static final String BTN_LOGIN_TEXT = "Login";
+    public static final String BTN_LOGOUT_TEXT = "Logout";
+}
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,24 +1,29 @@
 package sample;
 
+import constants.ApplicationConstants;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class Controller {
     public MenuItem mnuItmChangeCategories;
     public TextField txtFieldUserName;
     public PasswordField passwordFieldPwd;
     public Button btnLogin;
+    public TabPane tabPane;
+    public Tab tabPlay;
+    public Tab tabLogin;
 
     public void loginAction(ActionEvent event) {
-        if(btnLogin.getText().equals("Login")) {
-            btnLogin.setText("Logout");
+        if(btnLogin.getText().equals(ApplicationConstants.BTN_LOGIN_TEXT)) {
+            btnLogin.setText(ApplicationConstants.BTN_LOGOUT_TEXT);
         }
 
         else {
-            btnLogin.setText("Login");
+            btnLogin.setText(ApplicationConstants.BTN_LOGIN_TEXT);
         }
+    }
+
+    public void activateLoginTab(ActionEvent event) {
+        tabPane.getTabs().get(1).setDisable(false);
     }
 }
