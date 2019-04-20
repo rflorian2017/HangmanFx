1944416412b02c91d9d376562ba3dbc4c4d69588
rflorian2017 <roby_florian@yahoo.com>
2019-04-20 14:05:28 +0300

    added xml binding

--- src/helper/PlayerRegister.java
+++ src/helper/PlayerRegister.java
@@ -6,6 +6,7 @@ import model.Player;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import java.io.File;
 
 public class PlayerRegister {
@@ -30,21 +31,24 @@ public class PlayerRegister {
     }
 
     public Player readPlayer(String playerName) {
+        Player player = null;
+
         File playerFile = new File(ApplicationConstants.APP_FOLDER_DATA_PATH +
                 ApplicationConstants.PATH_SEPARATOR +
                 ApplicationConstants.PLAYERS_FOLDER_NAME +
                 ApplicationConstants.PATH_SEPARATOR +
                 playerName);
+
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(Player.class);
-            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 
-            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-            jaxbMarshaller.marshal(player, playerFile);
+            player = jaxbUnmarshaller.unmarshal(playerFile);
         } catch (JAXBException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
+
+        return player;
     }
 }
